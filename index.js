e0d3b4f51816495d1d25964efa793e8feaf53d4e
Diego <63005069+Dariing@users.noreply.github.com>
2024-08-22 23:28:30 -0600

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const clc = require('cli-color');
 
 const bot = new Discord.Client();
 
-const token = 'Njk0MDA0ODkzMzQxNDUwMjYw.XoJhrA.drJeJg8yR0Vx1AqghCctxACWnq4';
+const token = '';
 
 const PREFIX = '!cuasi ';
 
@@ -35,14 +35,6 @@ bot.on('message', async message => {
       message.channel.send('', {files: ['./img/teshin.png']});
     break;
 
-    case 'micodigo':
-      message.channel.send('Al estar vasado en muchos bot MI PADRE NO ES ORIGINAL PERO viva papiiii');
-    break;
-
-     case 'mipapi':
-      message.channel.send('Me creo DIEGO, alias DARK, pero trabajo como asesino con Enrique KING Gonzalez.');
-    break;
-
      case 'help':
       bot.commands.get('help').execute(message, args, bot);
     break;
@@ -55,14 +47,6 @@ bot.on('message', async message => {
        bot.commands.get('serverinfo').execute(message, args);
     break;
 
-    case 'tonto':
-      message.channel.send("S3gg, claro es buena persona y amigo pero las cosas como son es re pnd jajaja");
-    break;
-
-    case 'clanes':
-      message.channel.send("Acutual mente contamos con...... `30` clanes en la alianza");
-    break;
-
     case 'warn':
       bot.commands.get('warn').execute(message, args);
     break;
@@ -73,10 +57,6 @@ bot.on('message', async message => {
 
     case 'clear':
       bot.commands.get('clear').execute(message, args);
-    break;
-
-    case 'cuasi':
-      message.channel.send("Don cuasi o como se le conose como cuasi de sinaloa es un narcotraficante famoso se dise q actualmente el gana 30 millones diarios por su trabajos malos supera a cualquier narco y le gusta poner el comando ch!fuck cuando se emociona o se siente feliz con alguien... sus padres se llaman la reina del sur y el padre se llama el perrom mamon el nacio en 1850 antes de cristo el actualente es karateka es sinta negra supera a putini x100 contaria mas pero eso es de su vida privada....");
     break;
 
     case 'unban':
